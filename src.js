651cb40b9d4e85bdc861095ce7c258a71e228201
Yu-Tzu, Lin <linyutsu23@gmail.com>
2018-06-21 01:36:43 +0800

    針對序號不足錯誤訊息讓它還是可以跑(換頁沒測試過..)

--- src.js
+++ src.js
@@ -1,10 +1,19 @@
 var i = 0; 
+var pass = 0; 
+var page = 1; 
 
 var content = function (){
 	var tmp1, tmp2; 
 	tmp1 = document.getElementsByClassName("popup_main_prize")[0].innerText; 
-	tmp2 = document.getElementsByClassName("popup_main_prize_num")[0].innerText; 
-	console.log(tmp1 + "\t" + tmp2); 
+	if(tmp1=="序號不足，請等待官方補充序號，謝謝!!"){
+		tmp2 = ""; 
+		pass++;
+		i--; 
+	} 
+	else{
+		tmp2 = document.getElementsByClassName("popup_main_prize_num")[0].innerText; 
+		console.log(tmp1 + "\t" + tmp2);
+	}	 
 	document.getElementsByClassName("popup_close")[0].click()
 	i++; 
 	setTimeout(doLoop, 1500); 
@@ -15,7 +24,13 @@ var doLoop = function (){
 		console.log("總共領取了 " + i + " 個序號"); 
 		return; 
 	}
-    $('.prize_radio')[0].checked = true; 
+	//console.log("i = " + i + ", pass = " + pass + ", page = " + page + "; "); 
+	if(pass%10 == 0 && pass!=0){
+		console.log("下一頁"); 
+		GetUnexchangeItems(page+1);
+		page++;  
+	}
+    $('.prize_radio')[0+pass].checked = true; 
 	Exchange(); 
 	setTimeout(content, 1500); 
 }
